9af10c20094b5e0c94b3fe66765e477d46d06e89
marksikaundi <emmanuelmark117@gmail.com>
2022-03-07 20:54:56 +0200

    configuration

---
+++ sanity.js
@@ -0,0 +1,33 @@
+import {
+    createImageUrlBuilder,
+    createCurrentUserHook,
+    createClient
+} from "next-sanity"
+
+export const config = {
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    apiVersion: "2021-03-25",
+
+    useCdn: process.env.MODE_ENV === "production",
+};
+
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+
+
+export const useCurrentUser = createCurrentUserHook(config);
+
+
+
+
+
+
+
+
+
+
+
+
